7630bde9bcd15cc1d15b39eb9ca68403acacc616
AanandBhandari <aanandbhandari143@gmail.com>
2018-02-28 15:09:36 +0545

    port modified

--- server.js
+++ server.js
@@ -46,6 +46,6 @@ app.get('/project',(req,res)=>{
 		projectMessage: 'helloworld'
 	});
 });
-app.listen(3000, () =>{
+app.listen(port, () =>{
 	console.log(`the server is running now on port  ${port}`);
 });
